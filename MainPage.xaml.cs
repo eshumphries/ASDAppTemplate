83824ccd0e4c3bedabf41b6e4d913d2e1bb449ee
eshumphries <eshumphries@outlook.com>
2025-07-02 11:20:15 -0400

    Added comments and tweaked the XAML and C# code just a bit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,12 +13,14 @@ namespace ASDAppTemplate
         public MainPage()
         {
             InitializeComponent();
-
+            // Create the method to change page element size according to page size
             DisplayScrollViewer.SizeChanged += DisplayScrollViewer_SizeChanged;
         }
         private void DisplayScrollViewer_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+            // Store the right screen size
             double scale = Math.Min(e.NewSize.Width / 800, e.NewSize.Height / 600);
+            // Scale it just right
             DisplayGrid.RenderTransform = new ScaleTransform
             {
                 ScaleX = scale * 1.8,
